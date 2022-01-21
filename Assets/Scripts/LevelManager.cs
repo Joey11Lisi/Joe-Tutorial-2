608d7ddd8fe1744e7f31b78af04cf5fdc2272d21
Joey11Lisi <joey11lisi@knights.ucf.edu>
2022-01-21 13:47:28 -0500

    Challenge2

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -49,6 +49,10 @@ public class LevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
     }
 }
